4e4d8749fdbdd38496deef2e5280a65c03c6d732
Dhiraj45ch <158137074+Dhiraj45ch@users.noreply.github.com>
2024-06-05 12:12:41 +0530

    Add files via upload

---
+++ interchange.py
@@ -0,0 +1,6 @@
+#python program to interchange first and last elements in a list python.
+days=["mon","tue","wed","thu","sat"]
+days.insert(0, "sat")  
+days.append("mon")
+print("List after interchange : ", days)
+
---
+++ tuple.py
@@ -0,0 +1,3 @@
+#Write a  Python program to create a tuple of numbers and print it in reverse.
+numbers = (45,55,65,75,85)
+print("Reverse:", numbers[::-1])
